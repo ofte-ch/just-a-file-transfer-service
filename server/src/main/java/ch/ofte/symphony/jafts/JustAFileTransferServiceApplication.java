a5fe8af683cc171ae70380965bfb9b2ae2a17f05
Vy Tien <me@tienminhvy.com>
2026-01-07 09:05:08 +0700

    #2, init: setup the project with Spring

---
+++ server/src/main/java/ch/ofte/symphony/jafts/JustAFileTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package ch.ofte.symphony.jafts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustAFileTransferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustAFileTransferServiceApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/ch/ofte/symphony/jafts/JustAFileTransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ofte.symphony.jafts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustAFileTransferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
