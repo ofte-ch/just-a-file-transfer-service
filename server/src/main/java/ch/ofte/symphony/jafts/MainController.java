25e5e8c9772c967d7f251e098e9cc1ab4f22cbba
Vy Tien <me@tienminhvy.com>
2026-01-08 17:54:44 +0700

    #2, feat: add the default page

---
+++ server/src/main/java/ch/ofte/symphony/jafts/MainController.java
@@ -0,0 +1,16 @@
+package ch.ofte.symphony.jafts;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@ControllerAdvice
+@RequestMapping("")
+public class MainController {
+    @GetMapping("/**")
+    public String doGet() {
+        return "home.html";
+    }
+}
