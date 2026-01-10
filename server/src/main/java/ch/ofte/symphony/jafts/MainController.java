f2bf478f90a12afbac9692f107fbc14ac700ea91
1119-DuyNguyen <thanhduy191103@gmail.com>
2026-01-10 14:28:10 +0700

    init: setup layout

--- server/src/main/java/ch/ofte/symphony/jafts/MainController.java
+++ server/src/main/java/ch/ofte/symphony/jafts/MainController.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@ControllerAdvice
 @RequestMapping("")
 public class MainController {
-    @GetMapping("/**")
+    @GetMapping("")
     public String doGet() {
-        return "home.html";
+        return "home";
     }
 }
--- server/src/main/java/ch/ofte/symphony/jafts/config/SecurityConfiguration.java
+++ server/src/main/java/ch/ofte/symphony/jafts/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package ch.ofte.symphony.jafts.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -9,8 +10,45 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
-        return http.build();
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/api/**")
+                )
+//                .sessionManagement(session -> session
+//                                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+//                        // whether A user can log in from multiple devices/browsers at the same time
+////                        .maximumSessions(1)
+////                        .maxSessionsPreventsLogin(true)
+////                        .expiredUrl("/login?expired")
+//                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/css/**",
+                                "/js/**",
+                                "/static/**",
+                                "/images/**",
+                                "/favicon.ico",
+                                "/webjars/**",
+                                "/api/**"
+                        ).permitAll()
+                        .requestMatchers("/public/**", "/").permitAll()
+                        .anyRequest().authenticated()
+                )
+//                .formLogin(form -> form
+//                        .loginPage("/login")
+//                        .defaultSuccessUrl("/dashboard", true)
+//                        .permitAll()
+//                )
+//                .logout(logout -> logout
+//                        .logoutSuccessUrl("/login?logout")
+//                        .invalidateHttpSession(true)
+//                        .deleteCookies("JSESSIONID", "remember-me")
+//                        .addLogoutHandler(customLogoutHandler)
+//                        .permitAll()
+//                )
+//                .userDetailsService(userDetailsService)
+                .build();
     }
 }
